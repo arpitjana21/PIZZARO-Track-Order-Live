fe885c1cddc3baa8ea678859347df736ec4559a6
Arpit Jana <arpitjana2103@gmail.com>
2023-09-04 22:21:32 +0530

    Tailwind SetUP done

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,7 @@ function add(a, b) {
   return a + b;
 }
 var a = add(5, 7);
+alert('Hello');
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,4 @@ function add(a, b) {
     return a + b;
 }
 const a = add(5, 7);
+alert('Hello');
