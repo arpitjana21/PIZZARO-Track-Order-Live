24f029c0b3198cfd2740e459fa651f9f4c933340
Arpit Jana <arpitjana2103@gmail.com>
2023-09-17 16:21:13 +0530

    Crypto Middleware Added

--- app/http/middlewares/middleware.js
+++ app/http/middlewares/middleware.js
@@ -1,3 +1,7 @@
+const crypto = require('crypto');
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
 const userView = function (req, res, next) {
     if (req.user) {
         next();
@@ -52,12 +56,19 @@ const adminProtected = function (req, res, next) {
     }
 };
 
-function encode(originalString) {
-    return originalString;
+function encode(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString('hex');
 }
 
-function decode(encodedString) {
-    return encodedString;
+function decode(text) {
+    let encryptedText = Buffer.from(text, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
 }
 
 module.exports = {
